403aa61d5705fc3c094bc63b9ea6c56dffed5fdf
comquent <andreas.schoenfeld@comquent.de>
2017-07-06 21:32:40 +0200

    erste version

---
+++ src/main/java/com/comquent/App.java
@@ -0,0 +1,13 @@
+package com.comquent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
